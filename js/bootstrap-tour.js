529ca8200809d45629e9865d076fb0f9827ef971
Mike Crittenden <mikecrittenden@gmail.com>
2013-10-14 13:58:36 -0400

    Revert "fix going to front page on any step but the first"
    This reverts commit 7e3107452a9732de08d1595452966f464baa6160.

--- js/bootstrap-tour.js
+++ js/bootstrap-tour.js
@@ -28,6 +28,7 @@
         }
         t.addSteps([options])
       });
+        debugger;
 
       if (tourConfig.force && tourConfig.isFirstStep) {
         // Manually restart if "force" is true and we're on the path of the first step.
